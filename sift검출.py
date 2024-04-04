b9f8523cc64264f91282adb4b73b89f538867e3c
jayiuk <jayiuk987@gmail.com>
2024-04-04 15:30:49 +0900

    dfa

---
+++ sift검출.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  4 15:21:56 2024
+
+@author: jayiu
+"""
+
+import cv2 as cv
+img = cv.imread("C:/com_vision/source/image/mot_color70.jpg")
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+sift = cv.SIFT_create()
+kp, des = sift.detectAndCompute(gray, None)
+gray = cv.drawKeypoints(gray, kp, None, flags = cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv.imshow("sift", gray)
+
+cv.waitKey()
+cv.destroyAllWindows() 
